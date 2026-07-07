f510e761d5b5fa139ce7b77ab58d8288ee698027
Kanyadari Gnana vijayakanth <kanyadari.vijaykanth2004@gmail.com>
2026-07-07 13:11:36 +0530

    Add files via upload

---
+++ JavaFundamentals/ChangeCase.java
@@ -0,0 +1,14 @@
+public class ChangeCase {
+    public static void main(String[] args) {
+
+        char ch = 'a';   // Initialize the character
+
+        if (ch >= 'a' && ch <= 'z') {
+            System.out.println(ch + "->" + Character.toUpperCase(ch));
+        } else if (ch >= 'A' && ch <= 'Z') {
+            System.out.println(ch + "->" + Character.toLowerCase(ch));
+        } else {
+            System.out.println("Invalid Input");
+        }
+    }
+}
