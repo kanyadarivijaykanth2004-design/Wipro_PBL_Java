b434015499708f56125743098332248d7d15535d
Kanyadari Gnana vijayakanth <kanyadari.vijaykanth2004@gmail.com>
2026-07-07 14:17:55 +0530

    Add files via upload

---
+++ JavaFundamentals/EvenOddArray.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class EvenOddArray {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 0, 1, 0, 0, 1, 1};
+        int[] result = new int[arr.length];
+
+        int index = 0;
+
+        // Store even numbers first
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                result[index] = arr[i];
+                index++;
+            }
+        }
+
+        // Store odd numbers next
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                result[index] = arr[i];
+                index++;
+            }
+        }
+
+        System.out.println(Arrays.toString(result));
+    }
+}
