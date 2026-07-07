a8810a059524955ddf53c90dcfb11b709f231e19
Kanyadari Gnana vijayakanth <kanyadari.vijaykanth2004@gmail.com>
2026-07-07 13:55:34 +0530

    Add files via upload

---
+++ JavaFundamentals/ArrayCheck.java
@@ -0,0 +1,18 @@
+public class ArrayCheck {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 10, 34, 56, 7};
+        int search = 7;
+
+        int index = -1;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == search) {
+                index = i;
+                break;
+            }
+        }
+
+        System.out.println(index);
+    }
+}
