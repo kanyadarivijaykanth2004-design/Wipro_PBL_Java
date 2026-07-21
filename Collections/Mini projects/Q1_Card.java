0b4b4fb9701c101bd9531571a87e9bc32df10ebd
Kanyadari Gnana vijayakanth <kanyadari.vijaykanth2004@gmail.com>
2026-07-21 11:49:28 +0530

    Add files via upload

---
+++ Collections/Mini projects/Q1_Card.java
@@ -0,0 +1,11 @@
+package Collections.Mini
+
+public class Q1_Card {
+    String symbol;
+    int number;
+
+    Q1_Card(String symbol, int number) {
+        this.symbol = symbol;
+        this.number = number;
+    }
+}
---
+++ Collections/Mini projects/Q2Card.java
@@ -0,0 +1,12 @@
+package Collections.Mini
+
+public class Q2Card {
+
+    String symbol;
+    int number;
+
+    Q2Card(String symbol, int number) {
+        this.symbol = symbol;
+        this.number = number;
+    }
+}
